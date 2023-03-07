ce2cd602fe3fe76192a00b601057c6c5e069e63c
Bernardo Serra <bernardo.serra.pt@gmail.com>
2023-03-07 21:04:42 +0000

    DB Test Init

---
+++ src/test/kotlin/pt/isel/ls/sql/DBTest.kt
@@ -0,0 +1,41 @@
+package pt.isel.ls.sql
+import org.postgresql.ds.PGSimpleDataSource
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SqlTests {
+    private val dataSource = PGSimpleDataSource().also{it.setURL(System.getenv("JDBC_DATABASE_URL"))}
+
+
+    @Test
+    fun insert() {
+        dataSource.connection.use {connect ->
+            val stm = connect.prepareStatement("insert into courses(name) values ('LEIM');").executeQuery().also { it.next() }
+            assertEquals("Alice", stm.getString("name"))
+        }
+    }
+
+    @Test
+    fun update() {
+        dataSource.connection.use {connect ->
+            val stm = connect.prepareStatement("select * from students").executeQuery().also { it.next() }
+            assertEquals("Alice", stm.getString("name"))
+        }
+    }
+
+    @Test
+    fun select() {
+        dataSource.connection.use {connect ->
+            val stm = connect.prepareStatement("select * from students").executeQuery().also { it.next() }
+            assertEquals("Alice", stm.getString("name"))
+        }
+    }
+
+    @Test
+    fun delete() {
+        dataSource.connection.use {connect ->
+            val stm = connect.prepareStatement("select * from students").executeQuery().also { it.next() }
+            assertEquals("Alice", stm.getString("name"))
+        }
+    }
+}
