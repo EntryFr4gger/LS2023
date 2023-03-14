fb2cf6644636176d82d49a19aa81336e957aa7b1
Bernardo Serra <bernardo.serra.pt@gmail.com>
2023-03-14 22:52:42 +0000

    Sql Scripts Done

--- src/main/sql/addData.sql
+++ src/main/sql/addData.sql
@@ -1,3 +1,37 @@
-insert into courses(name) values ('LEIC');
-insert into students(course, number, name) values (1, 12345, 'Alice');
+BEGIN;
+    INSERT INTO Users(name, email)
+        VALUES ('Bernardo', 'bernardo@isel.pt'),
+               ('Marcorio', 'marcorio@isel.pt'),
+               ('Rafael', 'rafael@isel.pt');
+
+    INSERT INTO Token (token, user_id)
+        VALUES ('B123', 1),
+               ('M123', 2),
+               ('R123', 3);
+
+    INSERT INTO Board (name, description)
+        VALUES ('UmaBoard', 'Esta é a board um'),
+               ('OutraBoard', 'Esta é outra board');
+
+    INSERT INTO User_Board (user_id, board_id)
+        VALUES (1, 1),
+               (2, 2),
+               (3, 1);
+
+    INSERT INTO  List (name, board_id)
+        VALUES ('PrimeiraLista', 1),
+               ('SegundaLista', 1),
+               ('TerceiraLista', 2);
+
+    INSERT INTO Card (name, description, dueDate, board_id, list_id)
+        VALUES ('LS', 'Phase1', '23:44:59.903', 1, 1),
+               ('PC', 'Phase2', '23:44:59.903', 1, 1),
+               ('SI', 'Phase3', '23:44:59.903', 1, 2),
+               ('ALG', 'Phase4', '23:44:59.903', 2, 3),
+               ('IASA', 'Phase4', '23:44:59.903', 2, 3),
+               ('LAE', 'Phase4', '23:44:59.903', 2, 3);
+COMMIT;
+
+/*
 insert into students(course, number, name) select cid as course, 12346 as number, 'Bob' as name from courses where name = 'LEIC'
+ */
--- src/main/sql/createSchema.sql
+++ src/main/sql/createSchema.sql
@@ -1,13 +1,43 @@
-drop table if exists students;
-drop table if exists courses;
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Token CASCADE;
+DROP TABLE IF EXISTS Board CASCADE;
+DROP TABLE IF EXISTS User_Board CASCADE;
+DROP TABLE IF EXISTS List CASCADE;
+DROP TABLE IF EXISTS Card CASCADE;
 
-create table courses (
-  cid serial primary key,
-  name varchar(80)
+create table Users (
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(60) NOT NULL,
+    email   VARCHAR(320) UNIQUE NOT NULL CHECK ( email ~ '^[A-Za-z0-9+_.-]+@(.+)$')
 );
 
-create table students (
-  number int primary key,
-  name varchar(80),
-  course int references courses(cid)
+create table Token (
+    token   CHAR(36) PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table Board (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(30) NOT NULL,
+    description VARCHAR(1000)
+);
+
+create table User_Board (
+    user_id     INT NOT NULL REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    board_id    INT NOT NULL REFERENCES Board (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table List (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(60) NOT NULL,
+    board_id    INT NOT NULL REFERENCES Board (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table Card (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(60) NOT NULL,
+    description VARCHAR(1000),
+    dueDate     CHAR(12) NOT NULL CHECK ( dueDate ~ '^(?:[01]\d|2[0123])\:(?:[012345]\d)\:(?:[012345]\d)\.\d{3}$'),
+    board_id    INT NOT NULL REFERENCES Board (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    list_id     INT NOT NULL REFERENCES List (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
