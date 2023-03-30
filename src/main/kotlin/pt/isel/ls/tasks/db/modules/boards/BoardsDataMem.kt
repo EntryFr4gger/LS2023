482c773c61a053402be03156fcb41c013723fdb4
48268 - Marçorio Fortes <a48268@alunos.isel.pt>
2023-03-30 11:00:30 +0100

    Data mem

--- src/main/kotlin/pt/isel/ls/tasks/db/modules/boards/BoardsDataMem.kt
+++ src/main/kotlin/pt/isel/ls/tasks/db/modules/boards/BoardsDataMem.kt
@@ -7,15 +7,17 @@ import java.lang.Error
 import java.sql.Connection
 
 class BoardsDataMem(private val source: TasksDataStorage): BoardsDB {
+
     init {
         source.boards[1] = Board(1, "ISEL", "Cenas do 4 semestre do isel")
         source.boards[2] = Board(2, "Compras", "Ida ao supermercado")
         source.boards[3] = Board(3, "Limpeza", "O que falta limpar cá em casa")
         source.nextBoardId.addAndGet(3)
-        source.userBoard[1] = listOf(1)
-        source.userBoard[2] = listOf(2,3)
-    }
 
+
+        source.userBoard[1] = listOf(0)
+        source.userBoard[2] = listOf(1,2)
+    }
     override fun createNewBoard(conn: TransactionManager, name: String, description: String): Int {
         val id = source.nextBoardId.getAndIncrement()
 
@@ -28,7 +30,7 @@ class BoardsDataMem(private val source: TasksDataStorage): BoardsDB {
     //refazer
     //verificar se user existe
     override fun addUserToBoard(conn: TransactionManager, userId: Int, boardId: Int): Int {
-
+        
 
 
         if (source.userBoard.containsKey(userId)) {
@@ -42,10 +44,25 @@ class BoardsDataMem(private val source: TasksDataStorage): BoardsDB {
         return  if (source.userBoard[userId] != null) userId else  -1
     }
 
-    override fun getUserBoards(conn: TransactionManager, userId: Int): List<Board> =
-        source.userBoard[userId]!!.mapNotNull { source.boards[it] }
+    override fun getUserBoards(conn: TransactionManager, userId: Int): List<Board> {
+        val userBoard =  source.userBoard[userId]
+        return if (userBoard != null )
+             userBoard.mapNotNull { source.boards[it] }
+        else throw Error ("User with no boards")
+    }
 
     override fun getBoardDetails(conn: TransactionManager, boardId: Int): Board =
         source.boards[boardId] ?: throw Error("No board")
 
+    override fun isNewName(conn: TransactionManager, name: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasBoard(conn: TransactionManager, boardId: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasUserInBoard(conn: TransactionManager, userId: Int): Boolean {
+        TODO("Not yet implemented")
+    }
 }
