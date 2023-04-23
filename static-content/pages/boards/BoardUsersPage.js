d4bda77920c6af7ca386480dfd793cbea6e96ebb
Dragoral <dragoral.wizard@gmail.com>
2023-04-23 22:14:56 +0100

    Merge remote-tracking branch 'origin/main'

--- static-content/pages/boards/BoardUsersPage.js
+++ static-content/pages/boards/BoardUsersPage.js
@@ -1,26 +1,24 @@
 import buttonWithRef from "../components/ButtonWithRef.js";
+import {br, div, li, ul} from "../components/dom/domTags.js";
 
 
 async function BoardUsersPage(state) {
-    return document.createElement("div");
-    const list = document.createElement('ul');
-    const boards = state.body["boards"]
+    const items = ['id', 'name', 'email'];
+    const users = state.body['users']
 
-    boards.forEach(board => {
-        const boardItem = document.createElement('li');
-        boardItem.textContent = "Board: " + board["name"];
-        boardItem.appendChild(buttonWithRef("board details", `/#boards/${board["id"]}`))
-        list.appendChild(boardItem);
-    });
-
-
-    const container = document.createElement("div");
-    container.appendChild(list);
-    container.appendChild(buttonWithRef("Home", "/#"));
-    container.appendChild(document.createElement('br'))
-    container.appendChild(buttonWithRef("UserDetails", "/#users/1"));
-
-    return container;
+    return div(
+        ul(
+            ...users.map( user =>
+                    items.map(currentItem =>
+                        li(
+                            (1),//currentItem + " = " + user[currentItem]
+                            buttonWithRef("Card Details", `/#cards/1`)//${user['id']}
+                        )
+                    )
+            )
+        ),
+        br()
+    )
 }
 
 
