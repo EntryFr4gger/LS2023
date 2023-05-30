796fc12ab29e10e368606933ec3c6c21b3869631
Dragoral <dragoral.wizard@gmail.com>
2023-05-30 21:18:14 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(group = "org.postgresql", name = "postgresql", version = "42.+")
+    implementation(group = "org.http4k", name = "http4k-core", version = "4.40.+")
+    implementation(group = "org.http4k", name = "http4k-server-jetty", version = "4.40.+")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-json", version = "1.5.+")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-datetime", version = "0.4.+")
+    implementation(group = "org.slf4j", name = "slf4j-api", version = "2.0.0-alpha5")
+    testImplementation("org.testng:testng:7.1.0")
+    runtimeOnly(group = "org.slf4j", name = "slf4j-simple", version = "2.0.0-alpha5")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.5.20")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("io.rest-assured:kotlin-extensions:5.3.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation(kotlin("reflect"))
+}
+
+group = "pt.isel.ls.tasks.api.core"
+version = "1.0-SNAPSHOT"
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+        exceptionFormat = TestExceptionFormat.SHORT
+        showCauses = true
+        showExceptions = true
+        showStackTraces = true
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.register<Copy>("copyRuntimeDependencies") {
+    into("build/libs")
+    from(configurations.runtimeClasspath)
+}
+
+tasks.named<Jar>("jar") {
+    dependsOn("copyRuntimeDependencies")
+    manifest {
+        attributes["Main-Class"] = "pt.isel.ls.tasks.TasksServerKt"
+        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(" ") { it.name }
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
