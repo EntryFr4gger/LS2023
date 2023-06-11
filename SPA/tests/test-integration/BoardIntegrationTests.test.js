d2dd3e2de1b8e9b9c90cacece911420ea2c2d384
48268 - Marçorio Fortes <a48268@alunos.isel.pt>
2023-06-11 22:26:30 +0100

    integration test

--- SPA/tests/test-integration/BoardIntegrationTests.test.js
+++ SPA/tests/test-integration/BoardIntegrationTests.test.js
@@ -8,6 +8,8 @@ import {CreateListFetch} from "../../static-content/components/api/fetch/lists/C
 import {GetBoardUsersFetch} from "../../static-content/components/api/fetch/boards/GetBoardUsersFetch";
 import {GetBoardCardsArchivedFetch} from "../../static-content/components/api/fetch/boards/GetBoardCardsArchivedFetch";
 import {CreateCardFetch} from "../../static-content/components/api/fetch/cards/CreateCardFetch";
+import {SearchBoardsFetch} from "../../static-content/components/api/fetch/boards/SearchBoardsFetch";
+import {DeleteBoardFetch} from "../../static-content/components/api/fetch/boards/DeleteBoardFetch";
 
 jest.mock("../../../SPA/static-content/components/utils/storage/get-token.js", () => ({
     getUserToken: jest.fn(() => global.BToken),
@@ -28,7 +30,7 @@ const LName = "LName"
 
 const CName = "CName"
 const CDescription = "CDescription"
-describe("Users Integration Tests", () => {
+describe("Board Integration Tests", () => {
     beforeAll(async () => {
         const response = await CreateUserFetch(userName, email, password);
         global.BToken = "Bearer " + response.token;
@@ -37,8 +39,8 @@ describe("Users Integration Tests", () => {
         global.BoardID = board.id;
         const list = await CreateListFetch(LName,global.BoardID);
         global.ListID = list.id;
-        // const card = await CreateCardFetch(CName,CDescription,global.BoardID,global.ListID);
-        // global.CardID = card.id;
+        const card = await CreateCardFetch(CName,CDescription,global.BoardID,global.ListID);
+        global.CardID = card.id;
 
     })
 
@@ -62,12 +64,17 @@ describe("Users Integration Tests", () => {
 
     it("Get the list with the users of a board", async () => {
         const response = await GetBoardUsersFetch(global.BoardID)
-        expect(response.users).toEqual([{id:global.UID, name : userName, email : email, password: " "}])
+        expect(response.users).toEqual([{id:global.UID, name : userName, email : email, password: ""}])
     });
-    //
-    // it("Gets the list of a cards for a given board", async () => {
-    //     const response = await GetBoardCardsArchivedFetch(global.BoardID)
-    //     expect(response.cards).toEqual([{
-    //         id: global.ListID, name : CName, description : CDescription, boardId: global.BoardID, listId: global.ListID}])
-    // });
+
+    it("Search for the name of the board in the database.", async () => {
+        const response = await SearchBoardsFetch(BName,0,10)
+        expect(response.boards).toEqual([{id: global.BoardID, name: BName, description : BDescription}])
+    });
+
+    it("Delete a board", async () => {
+        const response = await DeleteBoardFetch(global.BoardID)
+        expect(response.message).toEqual()
+        expect(response.sucess).toBe(true)
+    });
 });
